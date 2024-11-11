399da5bb195bd8eb97e8ae9bbc7c572b83580108
Komiljonovdev <uzokdeveloper@gmail.com>
2024-11-11 21:57:37 +0500

    MINOR: Implemented php Curl

--- Currency.php
+++ Currency.php
@@ -1,7 +1,6 @@
 <?php
 class Currency{
     const CURRENCY_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
-
     public function __construct(){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, self::CURRENCY_API_URL);
